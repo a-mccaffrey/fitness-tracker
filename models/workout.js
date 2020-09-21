2455508fd9b97e9b8c1dd4c2b6f6b063d3f935f2
a-mccaffrey <aisling.s.mccaffrey@outlook.com>
2020-09-20 21:27:45 -0400

    Completed exercise

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "What type of exercise did you do?"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "What is the name of your exercise?"
+        },
+        duration: {
+          type: Number,
+          required: "How long did you exercise for in minutes?"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
